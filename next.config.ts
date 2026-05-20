6af4e89e89e764dc6bed25bb8583423378e3d558
rjwdr2w4td-maker <rjwdr2w4td-maker@users.noreply.github.com>
2026-05-20 16:53:07 +0800

    修复：添加 assetPrefix 适配子路径部署

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'build',
+  assetPrefix: '/huangjie.github.io',
   images: {
     unoptimized: true,
   },
