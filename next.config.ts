d8cef44a01946fce48ea069a2b04418cac33aae1
rjwdr2w4td-maker <rjwdr2w4td-maker@users.noreply.github.com>
2026-05-20 16:30:17 +0800

    Fix: remove assetPrefix for username.github.io repo

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'build',
-  assetPrefix: '/huangjie.github.io',
   images: {
     unoptimized: true,
   },
